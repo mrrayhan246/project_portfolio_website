48c0fca3ed20b6f6b8c646e0d6cf234724d58ae1
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-03 08:57:57 +0600

    Update

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technologies = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='projects/')
+    
+    def __str__(self):
+        return self.title
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    proficiency = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
