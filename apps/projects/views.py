cbde14b77cd32067ba472085c0914bbee32606c3
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-03 08:59:03 +0600

    Update

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Project, Skill
 
-# Create your views here.
+def home(request):
+    projects = Project.objects.all()
+    skills = Skill.objects.all()
+    return render(request, 'projects/home.html', {'projects': projects, 'skills': skills})
